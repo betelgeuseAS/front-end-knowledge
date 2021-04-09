73bef3cc88755e977bf88dbcce2c873cfa826dbe
DESKTOP-KIKRGFV\betel <betelgeuse.as@gmail.com>
2021-04-09 23:27:35 +0300

    init react & electron

---
+++ electron/main.js
@@ -0,0 +1,64 @@
+const {app, BrowserWindow} = require('electron');
+const isDev = require("electron-is-dev");
+const path = require ('path');
+const url = require ('url');
+
+function createWindow() {
+  // Create the browser window.
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+
+  const startUrl = process.env.ELECTRON_START_URL || url.format({
+    pathname: path.join(__dirname, '../index.html'),
+    protocol: 'file:',
+    slashes: true
+  });
+
+  mainWindow.loadURL(startUrl);
+  // mainWindow.loadURL(
+  //   isDev
+  //     ? "http://localhost:3000"
+  //     : `file://${path.join(__dirname, "../build/index.html")}`
+  // );
+
+  // Open the DevTools.
+  if (isDev) {
+    mainWindow.webContents.openDevTools(/*{ mode: "detach" }*/);
+  }
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', function () {
+    // On MacOS, they usually recreate the window in the application,
+    // after the icon in the dock has been clicked and there are no other open windows.
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  })
+});
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
+
+app.on("activate", () => {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.

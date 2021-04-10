3c3b256ebaeb05b6667a5476f7be778685e8ee22
DESKTOP-KIKRGFV\betel <betelgeuse.as@gmail.com>
2021-04-10 23:05:56 +0300

    update electrone file

--- electron/main.js
+++ electron/main.js
@@ -20,11 +20,7 @@ function createWindow() {
   });
 
   mainWindow.loadURL(startUrl);
-  // mainWindow.loadURL(
-  //   isDev
-  //     ? "http://localhost:3000"
-  //     : `file://${path.join(__dirname, "../build/index.html")}`
-  // );
+  // mainWindow.loadURL(isDev ? "http://localhost:3000" : `file://${path.join(__dirname, "../build/index.html")}`);
 
   // Open the DevTools.
   if (isDev) {
@@ -35,15 +31,7 @@ function createWindow() {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.whenReady().then(() => {
-  createWindow();
-
-  app.on('activate', function () {
-    // On MacOS, they usually recreate the window in the application,
-    // after the icon in the dock has been clicked and there are no other open windows.
-    if (BrowserWindow.getAllWindows().length === 0) createWindow();
-  })
-});
+app.whenReady().then(createWindow);
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
@@ -55,9 +43,7 @@ app.on("window-all-closed", () => {
 app.on("activate", () => {
   // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow();
-  }
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
 });
 
 // In this file you can include the rest of your app's specific main process

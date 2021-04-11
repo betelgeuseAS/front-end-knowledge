28a3faa5d05af60f622becdc6d9590b767f22a88
DESKTOP-KIKRGFV\betel <betelgeuse.as@gmail.com>
2021-04-11 18:15:47 +0300

    use sass preprocessor

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+import Dashboard from "./components/dashboard/Dashboard";
+
+import './App.sass';
 
 function App() {
+  // import UserStore from './database/stores/UserStore';
+  // const readAll = () => {
+  //   UserStore.fetchAll()
+  //     .then(allUsers => {
+  //       console.log(allUsers);
+  //     });
+  // };
+  // const create = () => {
+  //   const data = {password: 'kek', name: 'test'};
+  //   if (UserStore.validateSchemas(data)) {
+  //     UserStore.insert(data)
+  //       .then(result => {
+  //         // update view
+  //         console.log('Created: ', result);
+  //       });
+  //   } else {
+  //     return false;
+  //   }
+  // };
+  // const removeAll = () => {
+  //   UserStore.removeAll().then(result => {
+  //     console.log('Removed Items: ', result);
+  //   });
+  // };
+  // const update = () => {
+  //   UserStore.update({_id: "XRs5Re0hhtahUSFD"}, {name: 'test3'}).then(result => {
+  //     console.log('Updated: ', result);
+  //   });
+  // };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Dashboard />
     </div>
   );
 }

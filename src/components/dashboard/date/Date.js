db8ce31ff422d3e65faab71fa90c4b0e78259d88
DESKTOP-KIKRGFV\betel <betelgeuse.as@gmail.com>
2021-04-17 18:30:42 +0300

    date component

---
+++ src/components/dashboard/date/Date.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Date.sass';
+import Typography from "@material-ui/core/Typography";
+import Title from '../../Title';
+import moment from "moment";
+
+export default function Date() {
+  return (
+    <>
+      <Title>Date</Title>
+
+      <Typography align="center" component="div">
+        <Typography variant="subtitle2" component="div">{moment().format("dddd")}</Typography>
+        <Typography variant="h4" component="div">{moment().format("MMMM")}</Typography>
+        <Typography variant="h1" component="div" color="secondary">{moment().format("MM")}</Typography>
+        <Typography variant="h6" component="div">{moment().format("YYYY")}</Typography>
+      </Typography>
+    </>
+  );
+}

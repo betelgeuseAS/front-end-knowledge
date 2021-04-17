0a6e96b5506aa9d816f8481e4037fa8090cd87a5
DESKTOP-KIKRGFV\betel <betelgeuse.as@gmail.com>
2021-04-17 18:31:07 +0300

    use localize

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React, { Component } from "react";
-
 import Dashboard from "./components/dashboard/Dashboard";
-
-import { Localize } from "./components/utils/localization/Localize";
-import localization from "./components/utils/localization/localization";
-
+import { Localize } from "./components/utils/localize/Localize";
+import localization from "./components/utils/localize/localization";
 import './App.sass';
+import Box from '@material-ui/core/Box';
+import moment from "moment";
 
 class App extends Component {
   constructor(props) {
@@ -14,12 +13,67 @@ class App extends Component {
       language: ''
     };
 
-    localization.setLanguage('uk');
+    localization.setLanguage('en');
   }
 
   setLanguage = (language) => {
     localization.setLanguage(language);
     this.setState({language});
+    // localStorage.setItem('language', language);
+
+    // localization.getLanguage();
+    // localization.getInterfaceLanguage();
+
+    // localization.formatString(localization.currentDate, { //to format the passed string replacing its placeholders with the other arguments strings
+    //   month: localization.january,
+    //   day: 12,
+    //   year: 2018
+    // });
+    // localization.formatString(localization.onlyForMembers, <a href="http://login.com">{localization.login}</a>)
+    // localization.formatString(localization.iAmText, <b>{localization.bold}</b>)
+
+    this.setMomentLanguage(localization.getLanguage());
+  };
+
+  setMomentLanguage = (language) => {
+    //See https://momentjs.com/docs/#/customization/
+    moment.locale(language, {
+      months : localization.date.months_long,
+      monthsShort : localization.date.months_short,
+      monthsParseExact : true,
+      weekdays : localization.date.weekdays_long,
+      weekdaysShort : localization.date.weekdays_short,
+      weekdaysMin : localization.date.weekdays_min,
+      weekdaysParseExact : true,
+      longDateFormat : localization.date.moment.longDateFormat,
+      calendar : localization.date.moment.calendar,
+      relativeTime : localization.date.moment.relativeTime,
+      dayOfMonthOrdinalParse : /\d{1,2}(er|e)/,
+      ordinal : function (number, token) {
+        let b = number % 10;
+        let output = (~~ (number % 100 / 10) === 1) ? 'th' :
+          (b === 1) ? 'st' :
+            (b === 2) ? 'nd' :
+              (b === 3) ? 'rd' : 'th';
+        return number + output;
+      },
+      meridiemParse : /PM|AM/,
+      isPM : function (input) {
+        return input.charAt(0) === 'A';
+      },
+      // In case the meridiem units are not separated around 12, then implement
+      // this function (look at locale/id.js for an example).
+      // meridiemHour : function (hour, meridiem) {
+      //     return /* 0-23 hour, given meridiem token and hour 1-12 */ ;
+      // },
+      meridiem : function (hours, minutes, isLower) {
+        return hours < 12 ? localization.date.moment.pm : localization.date.moment.am;
+      },
+      week : {
+        dow : 1, // Monday is the first day of the week.
+        doy : 4  // Used to determine first week of the year.
+      }
+    });
   };
 
   // import UserStore from './database/stores/UserStore';
@@ -54,11 +108,11 @@ class App extends Component {
 
   render() {
     return (
-      <div className="app">
+      <Box component="div" className="app">
         <Dashboard/>
-        <Localize onSetLanguage={(language) => this.setLanguage(language)} />
-        <div>{localization.language}</div>
-      </div>
+        {/*<Localize onSetLanguage={(language) => this.setLanguage(language)} />*/}
+        {/*<div>{localization.language}</div>*/}
+      </Box>
     );
   }
 }

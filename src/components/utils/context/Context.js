ff96fc5b75a5ce3217625ae126b084391fb777eb
DESKTOP-KIKRGFV\betel <betelgeuse.as@gmail.com>
2021-05-07 21:02:06 +0300

    context

---
+++ src/components/utils/context/Context.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Context = React.createContext(null);
+
+export const ContextProvider = Context.Provider;
+// export const ContextConsumer = ThemeContext.Consumer;
+
+export default Context;
